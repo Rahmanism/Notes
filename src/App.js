75818813f57c1154d528fd89da614b93170dc2d8
Rahmanism <s.mostafa.rahmani@gmail.com>
2023-03-07 13:49:14 +0330

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container } from 'react-bootstrap'
+import { Navigate, Route, Routes } from 'react-router-dom'
+
 function App() {
-  return <h1>Hi</h1>
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Hi</h1>} />
+        <Route path="/new" element={<h1>New</h1>} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  )
 }
 
 export default App
