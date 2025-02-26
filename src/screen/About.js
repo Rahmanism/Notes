67778867169cca5b62493976de73eb9279d29a75
Rahmanism <s.mostafa.rahmani@gmail.com>
2025-02-26 10:39:18 +0330

    Add version to about page and update the about text.

--- src/screen/About.js
+++ src/screen/About.js
@@ -1,13 +1,17 @@
-import { Button, Stack } from "react-bootstrap"
-import { Link } from "react-router-dom"
+import { Button, Stack } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import packageJson from '../../package.json'
 
 export function About() {
   return (
     <>
       <h1>A Simple Note Taking App</h1>
+      <div className="mb-3">
+        Version: <b>{packageJson.version}</b>
+      </div>
 
       <p>
-        It's a deployment of this video tutorial:{" "}
+        It's a deployment of this video tutorial:{' '}
         <a href="https://www.youtube.com/watch?v=j898RGRw0b4">
           YouTube: Markdown Supported Note Taking
         </a>
@@ -19,10 +23,17 @@ export function About() {
         it supports markdown.
       </p>
       <p>
-        Note taking app saves the notes in the local storage of your browser.
+        ⚠️ Note taking app saves the notes in the{' '}
+        <b>
+          <u>local storage</u>
+        </b>{' '}
+        of your browser. You can export your notes to a JSON file (and soon-be-added import them).
       </p>
       <p>&nbsp;</p>
-      <Stack gap={2} direction="horizontal">
+      <Stack
+        gap={2}
+        direction="horizontal"
+      >
         <Link to="/">
           <Button variant="primary">Home</Button>
         </Link>
